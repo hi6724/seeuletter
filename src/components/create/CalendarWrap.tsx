9c5e32bafd42a78b4edd3cf8a7e75ab7993fec4f
jinjoo-jung <wjdwlswn23@naver.com>
2024-11-23 22:13:15 +0900

    feat: common 컴포넌트 생성

---
+++ src/components/create/CalendarWrap.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import styled from 'styled-components';
+import moment from 'moment';
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const CalendarWrap = () => {
+  const today = new Date();
+  const [date, setDate] = useState<Value>(today);
+
+  const handleDateChange = (newDate: Value) => {
+    setDate(newDate);
+  };
+
+  return (
+    <StyledCalendarWrapper>
+      <StyledCalendar
+        value={date}
+        onChange={handleDateChange}
+        calendarType="gregory"
+        formatDay={(locale, date) => moment(date).format('DD')}
+        next2Label={null} // +1년 & +10년 이동 버튼 숨기기
+        prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
+        minDetail="year" // 10년단위 년도 숨기기
+        locale="en-US"
+      />
+    </StyledCalendarWrapper>
+  );
+};
+
+export default CalendarWrap;
+
+const StyledCalendarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  .react-calendar {
+    border: none;
+    background: transparent;
+  }
+
+  .react-calendar__month-view__weekdays abbr {
+    text-decoration: none;
+    font-weight: 800;
+  }
+`;
+
+export const StyledCalendar = styled(Calendar)``;
